37575644ffa443fbe48cf6ec96db249cf64d57de
Anubis1960 <alexander.rk19@gmail.com>
2025-01-25 15:04:52 +0200

    test2

--- Server/app.py
+++ Server/app.py
@@ -2,13 +2,22 @@ import os
 from flask import Flask
 from src.util.extensions import socketio
 import logging
+from flask_mqtt import Mqtt
 import src.api.test
 
 # Flask app initialization
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
+app.config['MQTT_BROKER_URL'] = 'broker.hivemq.com'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_USERNAME'] = ''
+app.config['MQTT_PASSWORD'] = ''
+app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
+app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled
+
 
 # Bind socketio to the app
+mqtt = Mqtt(app)
 socketio.init_app(app)
 
 # Configure logging
@@ -21,4 +30,4 @@ logging.basicConfig(
 
 # Run the app
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=True, debug=True)
--- Server/src/api/test.py
+++ Server/src/api/test.py
@@ -1,17 +1,48 @@
 from flask_socketio import emit
 from src.util.extensions import socketio
+from app import mqtt
 
-@socketio.on('connect', namespace='/test')
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    print('Connected')
+    print(client)
+    print(userdata)
+    print(flags)
+    print(rc)
+    mqtt.subscribe('home/device/commands')
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    print('Message received')
+    print(client)
+    print(userdata)
+    print(message)
+    socketio.emit('mqtt_message', {'topic': message.topic, 'payload': message.payload.decode()})
+
+@socketio.on('connect')
 def handle_connect():
     print('Client connected')
-    emit('connected', {'data': 'You are connected!'})
-    emit('message', {'data': 'Welcome to the server!'})
+    emit('mqtt_message', {'topic': 'test', 'payload': 'test'})
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect')
 def handle_disconnect():
     print('Client disconnected')
 
-@socketio.on('message', namespace='/test')
-def handle_message(data):
-    print(f'Message received: {data}')
-    emit('message', {'data': f'Server response: {data}'})
+@socketio.on('mqtt_publish')
+def handle_mqtt_publish(data):
+    print('Publishing message')
+    print(data)
+    mqtt.publish(data['topic'], data['payload'])
+
+@socketio.on('mqtt_subscribe')
+def handle_mqtt_subscribe(data):
+    print('Subscribing to topic')
+    print(data)
+    mqtt.subscribe(data['topic'])
+
+@socketio.on('mqtt_unsubscribe')
+def handle_mqtt_unsubscribe(data):
+    print('Unsubscribing from topic')
+    print(data)
+    mqtt.unsubscribe(data['topic'])
+
